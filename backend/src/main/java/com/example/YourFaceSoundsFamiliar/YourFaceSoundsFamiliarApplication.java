249c62b9cb862d212708755c3d56abd7af5cbb0c
Blagovest Mihaylov <blagovestmihaylov@trading212.com>
2022-05-27 13:59:44 +0300

    added backend Spring boot template

---
+++ backend/src/main/java/com/example/YourFaceSoundsFamiliar/YourFaceSoundsFamiliarApplication.java
@@ -0,0 +1,14 @@
+package com.example.YourFaceSoundsFamiliar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourFaceSoundsFamiliarApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(YourFaceSoundsFamiliarApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/YourFaceSoundsFamiliar/YourFaceSoundsFamiliarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YourFaceSoundsFamiliar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourFaceSoundsFamiliarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
